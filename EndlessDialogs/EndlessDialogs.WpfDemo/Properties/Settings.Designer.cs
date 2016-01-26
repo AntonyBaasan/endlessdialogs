db767d1a38b6183377208e21afa90b40e6176e9f
AntonyBaasan <antony.baasan@gmail.com>
2016-01-26 13:04:11 -0700

    Remove WinFormDemo project. Added WpfDemo project.

--- EndlessDialogs/EndlessDialogs.WpfDemo/Properties/Settings.Designer.cs
+++ EndlessDialogs/EndlessDialogs.WpfDemo/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace EndlessDialogs.DemoWinForms.Properties
+namespace EndlessDialogs.WpfDemo.Properties
 {
 
 

e4f9b82aadf2100f41330eac9ff171126e327e3f
AntonyBaasan <antony.baasan@gmail.com>
2016-01-28 17:00:18 -0700

    Added Scene.cs methods created from IScene

--- EndlessDialogs/EndlessDialogs/Scene.cs
+++ EndlessDialogs/EndlessDialogs/Scene.cs
@@ -8,5 +8,39 @@ namespace EndlessDialogs
 {
     public class Scene : AbstractBasicObject, IScene
     {
+        public void AddConversation(IConversation conversation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddConversation(IEnumerable<IConversation> conversation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IConversation> GetConversations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadScene(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveConversation(IEnumerable<IConversation> conversation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveConversation(IConversation conversation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveScene(string filename)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
